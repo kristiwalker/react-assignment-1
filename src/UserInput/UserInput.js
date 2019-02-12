717002754c93f60e3404bc305c3b69fbafdf06d7
kristiwalker <kristiwalker22@gmail.com>
2019-02-12 16:27:34 -0500

    Completes tasks 1-3

---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6 */
+
+import React from 'react';
+
+const userInput = () => {
+    return (
+        <div className="UserInput">
+            <input type="text" />
+        </div>
+    )
+};
+
+export default userInput;
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+
+import React from 'react';
+
+const userInput = () => {
+    return (
+        <div className="UserInput">
+            <p>This is a test</p>
+            <p>Also a test</p>
+        </div>
+    )
+};
+
+export default userInput;
